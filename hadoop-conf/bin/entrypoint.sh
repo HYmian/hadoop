3988d7f05d4bae737021eeb46ae03563d26d2c99
mian <gopher.mian@outlook.com>
2016-09-29 10:31:11 +0800

    milestone add yarn-site.xml ha configration

--- hadoop-conf/bin/entrypoint.sh
+++ hadoop-conf/bin/entrypoint.sh
@@ -3,21 +3,21 @@
 if [ "$HADOOP_ROLE" == "NAMENODE1" ] ; then
   if [ ! -f /data/hdfs/runonce.lock ]; then
     if [ ! -d /data/hdfs/namenode ]; then
-      touch /data/hdfs/runonce.lock
       echo "NO DATA IN /data/hdfs/namenode"
       echo "FORMATTING NAMENODE"
       hdfs namenode -format || { echo 'FORMATTING FAILED' ; exit 1; }
     fi
+    touch /data/hdfs/runonce.lock
   fi
   export HADOOP_ROLE="NAMENODE1"
 elif [ "$HADOOP_ROLE" == "NAMENODE2" ] ; then
   if [ ! -f /data/hdfs/runonce.lock ]; then
     if [ ! -d /data/hdfs/namenode ]; then
-      touch /data/hdfs/runonce.lock
       echo "NO DATA IN /data/hdfs/namenode"
       echo "SYNCING DATA FROM NAMENODE1"
       hdfs namenode -bootstrapStandby
     fi
+    touch /data/hdfs/runonce.lock
   fi
   export HADOOP_ROLE="NAMENODE"
 fi
