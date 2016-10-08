06d05a2c9b1aa0003fbeb25dd6dc830c143712dc
HYmian <gopher.mian@outlook.com>
2016-10-08 16:29:05 +0800

    step update hadoop-conf/server and agent

--- hadoop-conf/tool/entrypoint.sh
+++ hadoop-conf/tool/entrypoint.sh
@@ -1,35 +1,37 @@
 #! /bin/bash
 
 if [ "$HADOOP_ROLE" == "NAMENODE1" ] ; then
-  if [ ! -f /data/hdfs/runonce.lock ]; then
-    if [ ! -d /data/hdfs/namenode ]; then
-      echo "NO DATA IN /data/hdfs/namenode"
-      echo "FORMATTING NAMENODE"
-      hdfs namenode -format || { echo 'FORMATTING FAILED' ; exit 1; }
-    fi
+    if [ ! -f /data/hdfs/runonce.lock ]; then
+        if [ ! -d /data/hdfs/namenode ]; then
+          echo "NO DATA IN /data/hdfs/namenode"
+          echo "FORMATTING NAMENODE"
+          hdfs namenode -format || { echo 'FORMATTING FAILED' ; exit 1; }
+        fi
     touch /data/hdfs/runonce.lock
-  fi
-  export HADOOP_ROLE="NAMENODE1"
-  hadoop-daemon.sh start zkfc
-  hdfs namenode
+    fi
+    export HADOOP_ROLE="NAMENODE1"
+    hadoop-daemon.sh start zkfc
+    hdfs namenode
 elif [ "$HADOOP_ROLE" == "NAMENODE2" ] ; then
-  if [ ! -f /data/hdfs/runonce.lock ]; then
-    if [ ! -d /data/hdfs/namenode ]; then
-      echo "NO DATA IN /data/hdfs/namenode"
-      echo "SYNCING DATA FROM NAMENODE1"
-      hdfs namenode -bootstrapStandby
-    fi
+    if [ ! -f /data/hdfs/runonce.lock ]; then
+      if [ ! -d /data/hdfs/namenode ]; then
+        echo "NO DATA IN /data/hdfs/namenode"
+        echo "SYNCING DATA FROM NAMENODE1"
+        hdfs namenode -bootstrapStandby
+      fi
     touch /data/hdfs/runonce.lock
-  fi
-  export HADOOP_ROLE="NAMENODE"
-  hadoop-daemon.sh start zkfc
-  hdfs namenode
+    fi
+    export HADOOP_ROLE="NAMENODE"
+    hadoop-daemon.sh start zkfc
+    hdfs namenode
 elif [ "$HADOOP_ROLE" == "HMASTER" ]; then
-  cp /etc/hosts /etc/hosts.old
-  sed -e "s/.*`hostname`/`curl -s http://rancher-metadata/latest/self/container/ips/0` `hostname`/g" /etc/hosts.old > /etc/hosts
-  hbase master start
+    cp /etc/hosts /etc/hosts.old
+    sed -e "s/.*`hostname`/`curl -s http://rancher-metadata/latest/self/container/ips/0` `hostname`/g" /etc/hosts.old > /etc/hosts
+    hbase-daemon.sh start master
+    server -n 10 -conf /bin/server-conf.yml
 elif [ "$HADOOP_ROLE" == "HREGIONSERVER" ]; then
-  cp /etc/hosts /etc/hosts.old
-  sed -e "s/.*`hostname`/`curl -s http://rancher-metadata/latest/self/container/ips/0` `hostname`/g" /etc/hosts.old > /etc/hosts
-  hbase regionserver start
+    cp /etc/hosts /etc/hosts.old
+    sed -e "s/.*`hostname`/`curl -s http://rancher-metadata/latest/self/container/ips/0` `hostname`/g" /etc/hosts.old > /etc/hosts
+    agent -s hmaster1:34616 -conf /bin/agent-conf.yml
+    hbase regionserver start
 fi
